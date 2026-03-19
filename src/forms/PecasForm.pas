634a66ba2f164f57d2347e73c2683006f2081f87
David <36790507+david-1983@users.noreply.github.com>
2026-03-18 23:46:58 -0300

    Create PecasForm.pas file in src/forms/ directory

---
+++ src/forms/PecasForm.pas
@@ -0,0 +1,23 @@
+unit PecasForm;
+
+interface
+
+uses
+  SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TPecasForm = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  PecasForm: TPecasForm;
+
+implementation
+
+{$R *.dfm}
+
+end.
